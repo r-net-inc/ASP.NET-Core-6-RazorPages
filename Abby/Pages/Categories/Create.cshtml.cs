350c90d700ff15b941d3378e5343b51b3858a89d
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-03-17 11:36:29 -0400

    Add Create Page

---
+++ Abby/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Abby.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
