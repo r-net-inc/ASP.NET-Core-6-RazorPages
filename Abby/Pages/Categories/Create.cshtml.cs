8ed3a9892770e6c510136cebe1d8f755564ffd59
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-03-17 12:07:53 -0400

    Added Create OnPost Action and binded properties

--- Abby/Pages/Categories/Create.cshtml.cs
+++ Abby/Pages/Categories/Create.cshtml.cs
@@ -1,15 +1,33 @@
+using Abby.Data;
 using Abby.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Abby.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
         public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost(/*Category category*/)
+        {
+            await _db.Categories.AddAsync(Category);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }
