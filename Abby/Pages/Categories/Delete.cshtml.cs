7675df220502be0e7e898ec209f10fc5640e79c2
Rana Sri <121699658+rana-sri@users.noreply.github.com>
2023-03-17 21:19:07 -0400

    Added Delete Action/Page

---
+++ Abby/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,39 @@
+using Abby.Data;
+using Abby.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Abby.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int id)
+        {
+            Category = _db.Categories.Find(id);
+            //Category = _db.Categories.First(c => c.Id == id);
+            //Category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //Category = _db.Categories.SingleOrDefault(c => c.Id == id);
+            //Category = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
+
+        }
+
+        public async Task<IActionResult> OnPost(/*Category category*/)
+        {
+            _db.Categories.Remove(Category);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
